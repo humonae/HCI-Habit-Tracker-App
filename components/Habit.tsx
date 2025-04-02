59c6fed923f9a71d193345492708992733244dbf
emeckley <emeckley@ufl.edu>
2025-04-01 20:23:43 -0400

    Add habit logging/unlogging, note button, and beginnings of streak counter

--- components/Habit.tsx
+++ components/Habit.tsx
@@ -1,10 +1,198 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, Alert } from "react-native";
+import ButtonWrapper from "@/components/ButtonWrapper";
+import React, { useState, useEffect } from "react";
+import * as SQLite from 'expo-sqlite';
+import uuid from 'react-native-uuid';
+
 
 interface HabitProps {
     name: string;   
+    id: string;
 }
 
 export default function Habit(props: HabitProps) {
+    const [db, setDB] = useState<SQLite.SQLiteDatabase>();
+    const [text, setText] = useState('hi');
+    const [logged, setLogged] = useState(false);
+    const [weekStreak, setWeekStreak] = useState(Array(7).fill(false));
+    const [streakNumber, setStreakNumber] = useState();
+
+    useEffect(() => {
+        const load = async () => {    
+            try {        
+                const db = await SQLite.openDatabaseAsync('databaseName');     
+                setDB(db);
+
+                //await db.execAsync(`
+                //    INSERT INTO HabitHistory (HabitID, DateCompleted) VALUES (1, '2024-03-29');
+                //`);
+
+            }
+            catch (err) {
+                console.error(err);
+            }
+        }
+        load();
+        //buildWeekStreak();
+    }, []);
+
+     const logHabit = async () => {
+        if (!db)
+            return;
+
+        try {
+            const date = (new Date()).toISOString().split('T')[0];
+
+            await db.execAsync(`
+                INSERT INTO HabitHistory (HabitID, DateCompleted) VALUES ('${props.id}', '${date}');
+            `);
+            setLogged(true);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    const unlogHabit = async () => {
+        if (!db)
+            return;
+
+        try {
+            const date = (new Date()).toISOString().split('T')[0];
+
+            await db.execAsync(`
+                DELETE FROM HabitHistory WHERE HabitID = '${props.id}' AND DateCompleted = '${date}';
+            `);
+            setLogged(false);
+
+            // update the weekly streak bar
+            setWeekStreak(prevState => {
+                const updatedStates = [...prevState];
+                updatedStates[0] = false;
+                return updatedStates;
+            });
+            setStreakNumber(streakNumber - 1);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    const habitIsPresent = async () => {
+        if (!db) {
+            return;
+        }
+
+        try {
+            const currDate = (new Date()).toISOString().split('T')[0];
+
+            const dates: any = await db.getAllAsync(`
+                SELECT * FROM HabitHistory WHERE HabitID = '${props.id}' AND DateCompleted = '${currDate}';
+            `);
+
+            if (dates && !dates.length) {
+                return false;
+            } else {
+                return true;
+            }
+
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    const calculateStreak = async () => {
+        if (!db) {
+            return;
+        }
+
+        try {
+            let currDate = new Date();
+            let currDateText = currDate.toISOString().split('T')[0];
+
+            const dates: any = await db.getAllAsync(`
+                SELECT * FROM HabitHistory WHERE HabitID = '${props.id}' AND DateCompleted = '${currDate}';
+            `);
+
+            let streakEnded = !(dates && !dates.length);
+            let count = 0;
+
+            while (!streakEnded) {
+                count++;
+                currDate.setDate(currDate.getDate() - 1);
+                currDateText = currDate.toISOString().split('T')[0];
+
+                const dates: any = await db.getAllAsync(`
+                    SELECT * FROM HabitHistory WHERE HabitID = '${props.id}' AND DateCompleted = '${currDate}';
+                `);
+
+                streakEnded = (dates && !dates.length);
+            }
+
+            setStreakNumber(count);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    const buildWeekStreak = async () => {
+        if (!db) {
+            return;
+        }
+
+        try {
+            let currDate = new Date();
+            let currDateText = currDate.toISOString().split('T')[0];
+
+            for (let i = 0; i < 7; i++) {
+                setWeekStreak(prevState => {
+                    const updatedStates = [...prevState];
+                    updatedStates[i] = false;
+                    return updatedStates;
+                });
+            }
+
+            for (let i = 0; i < 7; i++) {
+                console.log(currDateText);
+                console.log(props.id);
+                const dates: any = await db.getAllAsync(`
+                    SELECT * FROM HabitHistory WHERE HabitID = '${props.id}' AND DateCompleted = '${currDateText}';
+                `);
+                const present = !(dates.length === 0);
+                console.log(present);
+                console.log(dates);
+
+                setWeekStreak(prevState => {
+                    const updatedStates = [...prevState];
+                    updatedStates[i] = present;
+                    return updatedStates;
+                });
+
+                currDate.setDate(currDate.getDate() - 1);
+                currDateText = currDate.toISOString().split('T')[0];
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    const handleLogPress = async () => {
+        if (await habitIsPresent()) {
+            unlogHabit();
+        } else {
+            logHabit();
+            calculateStreak();
+            buildWeekStreak();
+        }   
+    }
+
+    const handleNotePress = async () => {
+
+    }
+
     return (
         <View style={{
             padding: 12,
@@ -13,7 +201,27 @@ export default function Habit(props: HabitProps) {
             borderRadius: 8,
             boxShadow: "0px 1px 4px 0px #00000010"
         }}>
-            <Text style={{fontSize: 24, fontWeight: "400"}}>{props.name}</Text>
+            <Text style={{fontSize: 24, fontWeight: "400", paddingBottom: 15}}>{props.name}</Text>
+            <View style={{position: 'absolute', top: 7, right: 10, color: (logged ? '#eee' : '#bbb'), backgroundColor: (logged ? '#2b4' : '#eee'), borderRadius: 100, width: 40, height: 40}}>
+                <Button title="✓" onPress={handleLogPress}/>
+            </View>
+            <View style={{position: 'absolute', top: 7, right: 60, backgroundColor: '#eee', borderRadius: 100, width: 40, height: 40}}>
+                <Button title="✐" onPress={handleNotePress}/>
+            </View>
+
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+              }}
+            >
+              {weekStreak.map((day, i) => (
+                <View key={i} style={{backgroundColor: (day ? '#2b4' : '#eee'), borderRadius: 100, width: 40, height: 40}}>
+                  <Text></Text>
+                </View>
+              ))}
+            </View>
         </View>
     )
 }
